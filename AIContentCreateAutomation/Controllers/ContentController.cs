d48c40996a81a1bb5d8857d2b2bd1a5b694055f2
hklong <hakhanhlong@gmail.com>
2025-10-15 17:32:30 +0700

    AI Content Create Automation

---
+++ AIContentCreateAutomation/Controllers/ContentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIContentCreateAutomation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+
+    }
+}
--- AIContentCreateAutomation/Program.cs
+++ AIContentCreateAutomation/Program.cs
@@ -1,4 +1,14 @@
+using AIContentCreateAutomation;
+using Microsoft.SemanticKernel;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var _configuration = provider.GetRequiredService<IConfiguration>();
+
+
+builder.Host.UseSerilog((hostingContext, loggerConfig) =>
+            loggerConfig.ReadFrom.Configuration(hostingContext.Configuration));
 
 // Add services to the container.
 
@@ -7,7 +17,26 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddSingleton<Kernel>(sk =>
+{
+    var kernelBuilder = Kernel.CreateBuilder();
+
+    string _geminiApiKey = _configuration["AppSettings:GeminiSettings:APIKey"];
+    string _geminiModelCode = _configuration["AppSettings:GeminiSettings:ModelCode"];
+
+    //kernelBuilder.AddOpenAIChatCompletion("llama3.2:3b", new Uri("http://localhost:11434/v1"), apiKey: "", serviceId: "gemma3");
+    kernelBuilder.AddGoogleAIGeminiChatCompletion(_geminiModelCode, apiKey: _geminiApiKey);
+
+    return kernelBuilder.Build();
+
+});
+
+
+builder.Services.Configure<AppSettings>(_configuration.GetSection("AppSettings"));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
